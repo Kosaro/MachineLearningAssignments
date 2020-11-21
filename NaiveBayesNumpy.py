9819016eafb7a53fa493f22c063f096d6f575b41
Kosaro <okosarkosarewicz@gmail.com>
2020-11-20 21:58:37 -0500

    Fixed input

--- NaiveBayesNumpy.py
+++ NaiveBayesNumpy.py
@@ -5,12 +5,12 @@
 
 from sys import argv
 import re
-from collections import Counter
+import numpy as np
 
 
 def main(train_path, test_path):
-    train_path = 'data/NaiveBayes/breast_cancer.train.txt'
-    test_path = 'data/NaiveBayes/breast_cancer.test.txt'
+    # train_path = 'data/NaiveBayes/breast_cancer.train.txt'
+    # test_path = 'data/NaiveBayes/breast_cancer.test.txt'
 
     train_path = 'data/NaiveBayes/led.train.txt'
     test_path = 'data/NaiveBayes/led.test.txt'
@@ -25,7 +25,7 @@ def main(train_path, test_path):
     test_labels, test_x = read_data(test_path, num_attributes, test_lines)
 
     # Train model
-    class_weights = train(train_x, train_labels, train_lines)
+    class_weights = train(train_x, train_labels)
 
     # predict labels
     predicted_labels = predict(train_x, class_weights)
@@ -58,19 +58,19 @@ def read_data(filepath, num_attributes, num_lines):
     :param filepath: input file
     :param num_attributes: number of attributes
     :param num_lines: number of lines
-    :return: data
+    :return: numpy ndarray with data
     """
-    labels = []
-    attributes = initialize_matrix(num_lines, num_attributes)
+    labels = np.ndarray(num_lines, dtype=np.int)
+    attributes = np.zeros((num_lines, num_attributes), dtype=np.int)
     with open(filepath) as f:
         for i, line in enumerate(f.readlines()):
-            labels.append(int(re.match(r'[-+]\d*', line).group()))
+            labels[i] = int(re.match(r'[-+]\d*', line).group())
             for index, value in re.findall(r'(\d*):(\d*)', line):
-                attributes[i][int(index) - 1] = int(value)
+                attributes[i, int(index) - 1] = int(value)
     return labels, attributes
 
 
-def train(data, labels, num_labels):
+def train(data, labels):
     """
     Trains a Naive Bayes classifier using Laplace smoothing.
 
@@ -81,23 +81,17 @@ def train(data, labels, num_labels):
     """
     class_weights = []
     # loop over classes/labels
-    for label in [-1,1]:
-        num_labels += 2
-        for l in labels:
-            if l == labels:
-                l += 1
+    for label in np.unique(labels):
+        label_count = np.sum(labels == label) + 2
         attribute_weights = []
         # loop over attributes
-        for i in range(len(data[0])):
-            counter = Counter()
-            for j in range(len(labels)):
-                if labels[j] == label:
-                    counter[data[j][i]] += 1 / num_labels
-            #print(counter)
-            attribute_weights.append(counter)
+        for i in range(data.shape[1]):
+            # get the unique values and respective counts for the attribute in rows with this label
+            values, counts = np.unique(data[labels == label, i], return_counts=True)
+            # Append dictionary with label probabilities for each value of this attribute
+            attribute_weights.append({value: (count + 1) / label_count for value, count in zip(values, counts)})
         class_weights.append(attribute_weights)
     return class_weights
-
 
 
 def predict(data, class_weights):
@@ -109,29 +103,12 @@ def predict(data, class_weights):
     attribute for each class. shape is num_classes x num_attributes.
     :return: predicted labels
     """
-    #probabilities = np.ndarray((data.shape[0], len(class_weights)))
-    #probabilities = [[0]*len(class_weights)]*len(data)
-    probabilities = initialize_matrix(len(data), len(class_weights))
+    probabilities = np.ndarray((data.shape[0], len(class_weights)))
     for i, row in enumerate(data):
         for r, label_weights in enumerate(class_weights):
-            #print(i, row)
-            #probabilities[i] = 0
-            probabilities[i][r] = product([label_weights[j].get(value, 0) for j, value in enumerate(row)])
-    #result = np.argmax(probabilities, axis=1)
-    #result[result == 0] = -1
-    result = []
-    for l,r in probabilities:
-        #print(l,r)
-        if l > r:
-            result.append(-1)
-        else:
-            result.append(1)
-    return result
-
-def product(list):
-    result = 1
-    for x in list:
-        result *= x
+            probabilities[i, r] = np.product([label_weights[j].get(value, 0) for j, value in enumerate(row)])
+    result = np.argmax(probabilities, axis=1)
+    result[result == 0] = -1
     return result
 
 
@@ -145,33 +122,12 @@ def print_summary(true_y, predicted_y):
     """
     matches = predicted_y[true_y == predicted_y]
     differences = predicted_y[true_y != predicted_y]
-    true_positives = 0
-    true_negatives = 0
-    false_positives = 0
-    false_negatives = 0
-    for true, predicted in zip(true_y, predicted_y):
-        if predicted==1:
-            if true == predicted:
-                true_positives += 1
-            else:
-                false_positives +=1
-        else:
-            if true == predicted:
-                true_negatives += 1
-            else:
-                false_negatives +=1
-
+    true_positives = np.count_nonzero(matches[matches == 1])
+    true_negatives = np.count_nonzero(matches[matches == -1])
+    false_positives = np.count_nonzero(differences[differences == 1])
+    false_negatives = np.count_nonzero(differences[differences == -1])
     print('%4s %4s %4s %4s' % (true_positives, false_negatives, false_positives, true_negatives))
 
-
-def initialize_matrix(x,y):
-    result = []
-    for i in range(x):
-        arr = []
-        for j in range(y):
-            arr.append(0)
-        result.append(arr)
-    return result
 
 if __name__ == '__main__':
     #main(argv[1], argv[2])
